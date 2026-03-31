e916e47275c6ebffe400d4e602897f37b844f816
Christa Egger <IHRE-GITHUB-EMAIL>
2026-03-31 23:50:05 +0200

    Revert "feat: add Borderline-Angehoerige React/Vite website"
    This reverts commit 20c74e6f03d8b70569548916311ec8d5ee8ca871.

--- borderline-angehoerige/client/src/components/layout/navigationData.ts
+++
@@ -1,9 +0,0 @@
-import {
-  primaryNavigationItems,
-  resourceNavigationItems,
-} from "@/domain/navigation";
-
-export { type NavigationItem as NavItem } from "@/domain/content-types";
-
-export const navItems = primaryNavigationItems;
-export const ressourcenItems = resourceNavigationItems;
