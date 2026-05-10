ce53352efc9cf03ddbf46429f4b62bd1510b27f1
Christa Egger <231125860+christaegger-dot@users.noreply.github.com>
2026-05-10 14:33:39 -0700

    Bust cached frontend assets

--- src/_data/site.js
+++ src/_data/site.js
@@ -17,6 +17,7 @@ const formatReviewedDate = (value) => {
 
 module.exports = {
   url: primaryUrl,
+  assetVersion: "2026-05-10-o1",
   buildYear: new Date().getFullYear(),
   title: "Bipolare Störung – Psychoedukation für Angehörige | PUK Zürich",
   org: "Psychiatrische Universitätsklinik Zürich",
